1e9a766625c1803b820e8ac4a2ea236989d1f317
Aryan Sharma <as9518403808@gmail.com>
2026-04-16 01:20:34 +0530

    Updated client and backend

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api").replace(/\/$/, "");
+const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || "https://bluewave-6y0s.onrender.com/api").replace(/\/$/, "");
 
 export default API_BASE_URL;
